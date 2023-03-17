1b30394acb5ca84159b0c203c8bb41db9646632b
shmll <shmllslmt@gmail.com>
2023-03-17 10:47:59 +0800

    Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,11 +16,65 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
   const DicePage({Key? key}) : super(key: key);
 
   @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDice = 1;
+  int rightDice = 2;
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              // Left Dice
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    randomize();
+                  });
+                  print('Left dice is pressed');
+                },
+                child: Image.asset(
+                  'images/dice$leftDice.png',
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              // Right Dice
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    randomize();
+                  });
+                  print('Right dice is pressed');
+                },
+                child: Image.asset(
+                  'images/dice$rightDice.png',
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void randomize() {
+    rightDice = Random().nextInt(6) + 1;
+    leftDice = Random().nextInt(6) + 1;
   }
 }
+
+
