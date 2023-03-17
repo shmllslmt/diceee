1a148f6555c38e89156f3f161034b3a47b630055
shmll <shmllslmt@gmail.com>
2023-03-17 08:07:35 +0800

    Initial Commit

---
+++ android/app/src/main/kotlin/com/shamilla/dice_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.shamilla.dice_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Center(child: Text('Dice')),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
